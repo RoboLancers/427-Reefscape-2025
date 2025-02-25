b793b23e5c94899a6d5672c84c40b03eeba302bb
Desmond <8842278@philasd.org>
2025-02-25 17:03:50 -0500

    we already have LED subsystem
    commented it all

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,33 +1,33 @@
-package frc.robot.subsystems;
+// package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.LEDPattern;
-import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.Color8Bit;
+// import edu.wpi.first.wpilibj.AddressableLED;
+// import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+// import edu.wpi.first.wpilibj.LEDPattern;
+// import edu.wpi.first.wpilibj.util.Color;
+// import edu.wpi.first.wpilibj.util.Color8Bit;
 
-public class LEDSubsystem extends SubsystemBase {
-    private static final int kPort = 0;
-    private static final int kLength = 0;
+// public class LEDSubsystem extends SubsystemBase {
+//     private static final int kPort = 0;
+//     private static final int kLength = 0;
 
 
-    private final AddressableLED m_led = new AddressableLED(9);
-    private final AddressableLEDBuffer m_buffer = new AddressableLEDBuffer(63);
+//     private final AddressableLED m_led = new AddressableLED(9);
+//     private final AddressableLEDBuffer m_buffer = new AddressableLEDBuffer(63);
 
-    public LEDSubsystem() {
-        LEDPattern red = LEDPattern.solid(Color.kRed); 
-    }
+//     public LEDSubsystem() {
+//         LEDPattern red = LEDPattern.solid(Color.kRed); 
+//     }
 
-@Override 
-public void periodic() {
-        //Periodically sends latest LED data to LED strip
-    m_led.setData(m_buffer);
-}
+// @Override 
+// public void periodic() {
+//         //Periodically sends latest LED data to LED strip
+//     m_led.setData(m_buffer);
+// }
 
-public Command runPattern(LEDPattern pattern) {
-    return run(() -> pattern.applyTo(m_buffer));
-    }
-}
+// public Command runPattern(LEDPattern pattern) {
+//     return run(() -> pattern.applyTo(m_buffer));
+//     }
+// }
