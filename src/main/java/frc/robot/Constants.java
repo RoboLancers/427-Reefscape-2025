2f39afda561d94af5c745d05bc467ae96ad9d67e
Jaely <10117985@philasd.org>
2025-02-26 18:40:45 -0500

    Removed algaeRemover Constants, changed kReverseSoftLimit to 90

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,7 +46,7 @@ public final class Constants {
   }
 
   public static final class VisionConstants {
-    public static final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+   public static final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
   public static final Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0));
   }
 
