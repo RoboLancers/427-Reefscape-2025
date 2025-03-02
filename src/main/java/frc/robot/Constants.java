5fe71b4d550153e2d1456dac7b7ede11604feace
BananasAmIRite <jasony1230@gmail.com>
2025-03-02 10:39:56 -0500

    Updated roller eject speed.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,7 +37,7 @@ public final class Constants {
     public static final int ROLLER_MOTOR_ID = 9;
     public static final int ROLLER_MOTOR_CURRENT_LIMIT = 40;
     public static final double ROLLER_MOTOR_VOLTAGE_COMP = 10;
-    public static final double ROLLER_EJECT_VALUE = 0.44;
+    public static final double ROLLER_EJECT_VALUE = 0.30;
   }
 
   public static final class OperatorConstants {
