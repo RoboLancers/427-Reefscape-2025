d13939170923f4a7f7b82dbca230916120ea91c3
Carl Stanton <carl.macy.stanton@protonmail.com>
2025-09-18 03:37:55 +0000

    playing around.

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mkdir /home/vscode
 cd /home/vscode
 wget 'https://github.com/wpilibsuite/vscode-wpilib/releases/download/v2025.2.1/vscode-wpilib-2025.2.1.vsix'
 wget https://github.com/Mechanical-Advantage/AdvantageScope/releases/download/v4.1.6/advantagescope-linux-x64-v4.1.6.deb
-sudo dpkg -i advantagescope-linux-x64-4.1.6.deb
+sudo dpkg -i /home/vscode/advantagescope-linux-x64-v4.1.6.deb
